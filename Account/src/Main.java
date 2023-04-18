559d714d0e5f2105ecc791ceb4fb58ca4e18a70b
ScottKYK <keguanyu@gmail.com>
2023-04-18 10:34:32 +0800

    Create Main.java

---
+++ Account/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    //模擬記帳APP
+    public static void main(String[] args) {
+        Detail detail = new Detail();
+        detail.showMenu();
+    }
+}
